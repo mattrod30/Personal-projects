4ea35e960355cd25cd0ce668ef9338437c7fb179
mattrod30 <matthewrodriguez1430@gmail.com>
2026-06-07 13:29:17 -0700

    Refactor clan-fitness.py for clarity and output fixes

--- clan-fitness.py
+++ clan-fitness.py
@@ -1,3 +1,17 @@
+from pyscript import document
+
+clan = {
+    "name": "Gladiators",
+    "members":[]
+}
+
+def display_members():
+    output = " "
+
+    for member in clan["members"]:
+        output += f"{member['name']}<br>"
+
+    document.querySelector("")
 users = [
     {"name": "Matthew", "goal": 150, "eaten": 150},
     {"name": "Alex", "goal": 160, "eaten": 120},
@@ -11,7 +25,7 @@ for user in users:
     
 sorted_users = sorted(users, key=lambda x: x["remaining"])
 
-print ("\n Leaderboard:")
+print ("\nLeaderboard:")
 for i, user in enumerate(sorted_users, start =1):
     print(f"Placement:{i} {user['name']} - {user['remaining']}g left")
 
@@ -22,9 +36,9 @@ for i, user in enumerate(sorted_users, start =1):
 
 print ("\nWinners:")
 for user in winners:
-    print(f"{user['name']} did that shi")
+    print(f"{user['name']} wins")
 
 print ("\nLosers:")
 for user in losers:
-    print(f"{user['name']} is a bitch")
+    print(f"{user['name']} must do a forfeit")
 print ("\n")
