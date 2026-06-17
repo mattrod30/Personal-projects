8a654f22461a63e321bc3e17e0b97a72067bb094
mattrod30 <matthewrodriguez1430@gmail.com>
2026-06-17 11:12:55 -0700

    Add Player class for platformer game
    Implement basic player class with movement and collision detection.

---
+++ platformer.py
@@ -0,0 +1,49 @@
+import pygame
+
+BLACK  = (0, 0, 0)
+WHITE = (255, 255, 255)
+GREEN = (0, 255, 0)
+RED = (255, 0, 0)
+BLUE = (0, 0, 255)
+
+SCREEN_WIDTH = 800
+SCREEN_HEIGHT = 500
+
+class Player(pygame.sprite.Sprite):
+    
+    def __init__(self):
+
+        super().__init__()
+
+        width = 40
+        height = 60
+        self.image = pygame.Surface([width, height])
+        self.image.fill(RED)
+
+        self.rect = self.image.get_rect()
+
+        self.change_x = 0
+        self.change_y = 0
+
+        self.level = None
+
+    def update(self):
+        self.calc_grav()
+
+        self.rect.x += self.change_x
+
+        block_hit_list = pygame.sprite.spritecollide(self, self.level.platform_list, False)
+        for block in block_hit_list:
+            if self.change_x > 0:
+                self.rect.right = block.rect.left
+            elif self.change_x < 0:
+                self.rect.left = block.rect.right
+
+        self.rect.y += self.change_y
+
+        block_hit_list = pygame.sprite.spritecollide(self, self.level.platform_list, False)
+        for block in block_hit_list:
+            if self.change_y > 0:
+                self.rect.bottom = block.rect.top
+            elif self.change_y < 0:
+                self.rect.top = block.rect.bottom
