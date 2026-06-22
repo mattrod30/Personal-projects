4e3191a49e018bacb7e5a2a680f999aa149ef685
mattrod30 <matthewrodriguez1430@gmail.com>
2026-06-22 16:22:23 -0700

    Refactor hangman game logic and improve input validation

--- hangman.py
+++ hangman.py
@@ -62,25 +62,33 @@ HANGMAN_STAGES = [
 word_collection = ["Banana", "Orange", "Grape", "Prime", 
                    "Game", "Basketball", "Soccer"]
 
-word_choice = random.choice(word_collection).lower()
-stage_tracker = 0
-    
-while True:
-    letter_guess = input("Pick a letter to guess for the word: ").lower()
+print ("Let's play Hangman!")
+correct_word = random.choice(word_collection).lower()
+blank_spaces = ["_"] * len(correct_word)
+guess_attempts = 6
+stage = 0
+while guess_attempts > 0 and "_" in blank_spaces:
+    letter_guess = input("Guess a letter: ").lower()
 
-    if len(letter_guess) > 1:
-        print("You must only guess a letter!")
+    if len(letter_guess) > 1 or not letter_guess.isalpha():
+        print ("Must enter a single letter only")
+        continue
 
-    if letter_guess in word_choice:
-        print(f"Yes, there is a {letter_guess} in this word.")
-        
-    matched_indices = [i for i, letter in enumerate(word_choice) if letter == letter_guess]
-    
+    if letter_guess in correct_word:
+        print(f"Yes, there is a {letter_guess} in this word")
+        for i, letter in enumerate (correct_word):
+            if letter == letter_guess:
+                blank_spaces[i] = letter_guess
+                print (" ".join(blank_spaces))
     else:
-        print (f"There is no {letter_guess} in this word")
-        stage_tracker += 1
-        print (HANGMAN_STAGES[stage_tracker])
+        print(f"Sorry, there is no {letter_guess} in this word")
+        guess_attempts -= 1
+        stage += 1
+        print (HANGMAN_STAGES[stage])
+        print (f"Attempts left: {guess_attempts}")
 
-    if stage_tracker == 6:
-        print ("Game over, better luck next time!")
-        break
+if "_" not in blank_spaces:
+    print(f"You got the word right, nice job! The word was {correct_word}")
+else:
+    print(f"Game over, better luck next time! The correct word was {correct_word}")
+  
